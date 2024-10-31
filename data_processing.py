ead7d94cf5b7ebbf26d9d828e943b8d293349299
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2024-10-31 16:06:15 +0700

    OOP implemented

--- data_processing.py
+++ data_processing.py
@@ -1,53 +1,58 @@
 import csv, os
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+class Travel:
+    def __init__(self, dict_list=[]) -> None:
+        self.dict_list = dict_list
+
+    def filter(self, condition):
+        filtered_list = []
+        for item in self.dict_list:
+            if condition(item):
+                filtered_list.append(item)
+        self.dict_list = filtered_list
     
-cities = []
-with open(os.path.join(__location__, 'Cities.csv')) as f:
+    def aggregate(self, aggregation_key, aggregation_function):
+        out = []
+        for item in self.dict_list:
+            out.append(float(item[aggregation_key]))
+        return aggregation_function(out)
+
+# get data
+_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+_cities = []
+with open(os.path.join(_location, 'Cities.csv')) as f:
     rows = csv.DictReader(f)
     for r in rows:
-        cities.append(dict(r))
-
-countries = []
-with open(os.path.join(__location__, 'Countries.csv')) as f:
+        _cities.append(dict(r))
+_countries = []
+with open(os.path.join(_location, 'Countries.csv')) as f:
     rows = csv.DictReader(f)
     for r in rows:
-        countries.append(dict(r))
+        _countries.append(dict(r))
 
-def filter(condition, dict_list):
-    filtered_list = []
-    for item in dict_list:
-        if condition(item):
-            filtered_list.append(item)
-    return filtered_list
-
-def aggregate(aggregation_key, aggregation_function, dict_list):
-    out = []
-    for item in dict_list:
-        out.append(float(item[aggregation_key]))
-    return aggregation_function(out)
+# init class
+italy = Travel(_cities)
+sweden = Travel(_cities)
+italy.filter(lambda x: x['country'] == 'Italy')
+sweden.filter(lambda x: x['country'] == 'Sweden')
 
 _avg = lambda x: sum(x) / len(x)
 _max = lambda x: max(x)
 _min = lambda x: min(x)
 
-scope_data1 = filter(lambda x: x['country'] == 'Italy', cities)
-scope_data2 = filter(lambda x: x['country'] == 'Sweden', cities)
-
 # - print the average temperature for all the cities in Italy
 print("The average temperature for all the cities in Italy is: ")
-print(aggregate("temperature", _avg, scope_data1))
+print(italy.aggregate("temperature", _avg))
 print()
 # - print the average temperature for all the cities in Sweden
 print("The average temperature of all the cities in Sweden:")
-print(aggregate("temperature", _avg, scope_data2))
+print(sweden.aggregate("temperature", _avg))
 print()
 # - print the min temperature for all the cities in Italy
 print("The average temperature of all the cities in Italy:")
-print(aggregate("temperature", _min, scope_data1))
+print(italy.aggregate("temperature", _min))
 print()
 # - print the max temperature for all the cities in Sweden
 print("The average temperature of all the cities in Sweden:")
-print(aggregate("temperature", _max, scope_data2))
+print(sweden.aggregate("temperature", _max))
 print()
